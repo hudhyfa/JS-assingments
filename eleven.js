11bc9c3cc3fe31c8dad53fa810769b61eb723e5a
hudhyfa <hudhyfahbk@gmail.com>
2023-09-18 20:16:24 +0530

    eleven done

--- eleven.js
+++ eleven.js
@@ -2,10 +2,11 @@ let arrInput = prompt("enter array values with , b/w");
 let arr = arrInput.split(",");
 
 let evenCount = 0;
-for(let i=0;i<arr.length;i++){
-  if(arr[i]%2==0){
+
+arr.forEach((i)=>{
+  if(i % 2 == 0){
     evenCount+=1;
   }
-}
+})
 
 console.log("even count in the array : "+ evenCount);
