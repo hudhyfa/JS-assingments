c0b9dcc5bc0bb98a2bcba50dbf373108127bc494
hudhyfa <hudhyfahbk@gmail.com>
2023-09-25 10:25:09 +0530

    28 done

---
+++ twentyeight.js
@@ -0,0 +1,12 @@
+class Car{
+    constructor(name,mileage,speed){
+      this.name=name;
+      this.mileage=mileage;
+      this.speed=speed
+      console.log(name);
+      console.log(mileage);
+      console.log(speed);  
+    }
+  }
+  const obj=new Car("ferrari",8,330)
+  
